8f02b05a9d8476b9a29c19ef01a84f261610c3eb
BaalDaruke <hskaring@hotmail.com>
2025-04-26 02:36:36 +0900

    v004

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // ❗ 빌드시 ESLint 무시!
+    },
+  };
+  
+  module.exports = nextConfig;
+  
